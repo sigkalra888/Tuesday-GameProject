230a029b2d813c965dcd4be8e66bc16a5638a5d5
kazuya <yuuki0523yata@gmail.com>
2019-04-24 03:59:11 +0900

    ・敵が触れた時に拠点の耐久が減る部分完了 ・拠点の耐久がなくなったらgameover処理完了 ・Title、Resultのsceneの追加(GAMEOVERの処理の時に必要なため)

---
+++ 火曜5.6限プロジェクト(プロトタイプ)/Assets/Scripts/BaseController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BaseController : MonoBehaviour
+{
+    [SerializeField] private GameObject R_Shiro;
+    [SerializeField] private GameObject L_Shiro;
+
+    [SerializeField] private int CastleLife;
+    void Start()
+    {
+
+    }
+    
+    void Update()
+    {
+        Debug.Log(CastleLife);　//確認用
+        //拠点の耐久がなくなったらgameover
+        if (CastleLife == 0)
+        {
+            Destroy(this.gameObject);
+            SceneManager.LoadScene("Result");
+        }
+    }
+    //Enemyが触れた時に拠点の耐久を減らす
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.transform.name == "Enemy")
+        {
+            CastleLife = CastleLife - 10;
+        }
+    }
+}

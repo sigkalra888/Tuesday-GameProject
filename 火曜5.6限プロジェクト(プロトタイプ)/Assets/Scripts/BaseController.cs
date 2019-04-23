6153051e8bbe0cb095a833ce948722db2e8b4faf
kazuya <yuuki0523yata@gmail.com>
2019-04-24 04:39:19 +0900

    ・scene遷移系統をまとめるようにscenemanagerの作成 ・見やすいようにスクリプトを書き換え

--- 火曜5.6限プロジェクト(プロトタイプ)/Assets/Scripts/BaseController.cs
+++ 火曜5.6限プロジェクト(プロトタイプ)/Assets/Scripts/BaseController.cs
@@ -8,21 +8,10 @@ public class BaseController : MonoBehaviour
     [SerializeField] private GameObject R_Shiro;
     [SerializeField] private GameObject L_Shiro;
 
-    [SerializeField] private int CastleLife;
-    void Start()
-    {
-
-    }
-    
+    [SerializeField] public int CastleLife;
     void Update()
     {
-        Debug.Log(CastleLife);　//確認用
-        //拠点の耐久がなくなったらgameover
-        if (CastleLife == 0)
-        {
-            Destroy(this.gameObject);
-            SceneManager.LoadScene("Result");
-        }
+        Debug.Log(CastleLife); //確認用
     }
     //Enemyが触れた時に拠点の耐久を減らす
     private void OnTriggerEnter2D(Collider2D col)

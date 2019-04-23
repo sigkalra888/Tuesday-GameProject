640d25318b927df27a1cbb618f97078bd22618d9
kazuya <yuuki0523yata@gmail.com>
2019-04-23 16:43:36 +0900

    ファイルのアップ

---
+++ 火曜5.6限プロジェクト(プロトタイプ)/Assets/Scripts/BottunBlink.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//ネット参照・よくある点滅するやつ
+public class BottunBlink : MonoBehaviour
+{
+    //public
+    public float speed = 1.0f;
+    //private
+    private Text text;
+    private Image image;
+    private float time;
+    private enum ObjType
+    {
+        TEXT,
+        IMAGE
+    };
+    private ObjType thisObjType = ObjType.TEXT;
+
+    private void Start()
+    {
+        //アタッチしてるオブジェクトを判別
+        if (this.gameObject.GetComponent<Image>())
+        {
+            thisObjType = ObjType.IMAGE;
+            image = this.gameObject.GetComponent<Image>();
+        }
+        else if(this.gameObject.GetComponent<Text>())
+        {
+            thisObjType = ObjType.TEXT;
+            text = this.gameObject.GetComponent<Text>();
+        }
+    }
+    private void Update()
+    {
+        //オブジェクトのAlpha値を更新
+        if (thisObjType == ObjType.IMAGE)
+        {
+            image.color = GetAlphaColor(image.color);
+        }
+        else if (thisObjType == ObjType.TEXT)
+        {
+            text.color = GetAlphaColor(text.color);
+        }
+    }
+    Color GetAlphaColor(Color color)
+    {
+        time += Time.deltaTime * 5.0f * speed;
+        color.a = Mathf.Sin(time) * 0.5f + 0.5f;
+
+        return color;
+    }
+}
---
+++ 火曜5.6限プロジェクト(プロトタイプ)/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb2d;
+
+    [SerializeField] private int hori;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb2d.velocity = new Vector2(-hori, 0);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb2d.velocity = new Vector2(hori, 0);
+        }
+    }
+}

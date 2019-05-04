e1dde4dec0ecaeb1105295ebdedc6d05b09940b6
sigkalra <sigkalra8888@gmail.com>
2019-05-04 18:06:34 +0900

    [Update] Lightの制御完了(仮状態)　マウスに追従するのと角度制限を付けました

--- 火曜5.6限プロジェクト(プロトタイプ)/Assets/Shimojima/Scripts/Light.cs
+++ 火曜5.6限プロジェクト(プロトタイプ)/Assets/Shimojima/Scripts/Light.cs
@@ -4,29 +4,66 @@ using UnityEngine;
 
 public class Light : MonoBehaviour
 {
-    float m_posx;
-    float m_posy;
     private int _myDir;
+    private float _eulerAngle;
+    private bool _angleCheck;
     [SerializeField]
-    private PlayerControllerTest pct;
+    private GameObject pct;
+    private PlayerControllerTest _pct;
+    [SerializeField]
+    private AngleCheck angC;
     void Start()
     {
-        pct = pct.GetComponent<PlayerControllerTest>();
+        _pct = pct.GetComponent<PlayerControllerTest>();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        _myDir = pct.myDir;
+        transform.localPosition = new Vector3(pct.transform.localPosition.x,-3.9f,0);
+        _eulerAngle = angC.GetComponent<AngleCheck>().eulerAngle;
+        _angleCheck = angC.GetComponent<AngleCheck>().angleCheck;
+        _myDir = _pct.myDir;
+        Debug.Log(_myDir);
 
+        if (_myDir == 1)
+        {
+            LookLeft();
+        }
+        else if (_myDir == 0)
+        {
+            LookRight();
+        }
+    }
+
+    private void LookRight()
+    {
         var pos = Camera.main.WorldToScreenPoint(transform.localPosition);
         var rotation = Quaternion.LookRotation(Vector3.forward, Input.mousePosition - pos);
 
-        if (transform.localEulerAngles.z >= 270 && transform.localEulerAngles.z <= 360 || transform.localEulerAngles.z == 0)
+        if (_eulerAngle < 360 && _eulerAngle > 270)
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+
+        if (_angleCheck)
         {
             transform.localRotation = rotation;
         }
-        
+    }
 
+    private void LookLeft()
+    {
+        var pos = Camera.main.WorldToScreenPoint(transform.localPosition);
+        var rotation = Quaternion.LookRotation(Vector3.forward, Input.mousePosition - pos);
+
+        if (_eulerAngle > 0 && _eulerAngle < 180)
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+
+        if (_angleCheck)
+        {
+            transform.localRotation = rotation;
+        }
     }
 }

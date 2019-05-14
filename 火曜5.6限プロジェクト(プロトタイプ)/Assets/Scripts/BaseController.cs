8ff925ae67d3d3ebd29ea39427d75deb6c3aa888
kazuya <yuuki0523yata@gmail.com>
2019-05-14 18:28:16 +0900

    センターの拠点の追加

--- 火曜5.6限プロジェクト(プロトタイプ)/Assets/Scripts/BaseController.cs
+++ 火曜5.6限プロジェクト(プロトタイプ)/Assets/Scripts/BaseController.cs
@@ -7,11 +7,14 @@ public class BaseController : MonoBehaviour
 {
     [SerializeField] private GameObject R_Shiro;
     [SerializeField] private GameObject L_Shiro;
+    [SerializeField] private GameObject C_shiro;
 
-    [SerializeField] public int _L_castleLife;
-    [SerializeField] public int _R_castleLife;
+    public int _L_castleLife;
+    public int _R_castleLife;
+    public int _C_castleLife;
+    
     private float _time;
-    public float _targettime;
+    public float[] _targettime = new float[2];
     void Update()
     {
         // Debug.Log(CastleLife); //確認用
@@ -19,16 +22,20 @@ public class BaseController : MonoBehaviour
         //仮のタイムでの作成
         _time += Time.deltaTime;
         //Debug.Log(_time);
-        if (_time >= _targettime)
+        if (_time >= _targettime[0])
         {
             L_Shiro.gameObject.SetActive(true);
+        }
+        if (_time >= _targettime[1])
+        {
+            C_shiro.gameObject.SetActive(true);
         }
     }
     public void CastleLife()
     {
-        if (_R_castleLife == 0 || _L_castleLife == 0)
+        if (_R_castleLife == 0 || _L_castleLife == 0 || _C_castleLife == 0)
         {
-            UnityEngine.SceneManagement.SceneManager.LoadScene("Result");
+            SceneManager.LoadScene("Result");
         }
     }
     //プレイヤーが触れたらライトを充電する

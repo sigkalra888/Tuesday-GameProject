0a82a0f2861ef7ea4c311f773f3055c75dabd1fd
kazuya <yuuki0523yata@gmail.com>
2019-05-14 15:47:28 +0900

    コミット漏れ

--- 火曜5.6限プロジェクト(プロトタイプ)/Assets/Scripts/SceneManager.cs
+++ 火曜5.6限プロジェクト(プロトタイプ)/Assets/Scripts/SceneManager.cs
@@ -5,13 +5,4 @@ using UnityEngine.SceneManagement;
 
 public class SceneManager : MonoBehaviour
 {
-    [SerializeField] private BaseController _baseControllers;
-    void Update()
-    {
-        //拠点の耐久が減った時の画面遷移処理
-        if (_baseControllers.CastleLife == 0)
-        {
-            UnityEngine.SceneManagement.SceneManager.LoadScene("Result");
-        }
-    }
 }

f527e53ef178f0b2871bc4c8542f7d22a79e1359
kazuya <yuuki0523yata@gmail.com>
2019-06-09 23:09:58 +0900

    seとbgmのmanagerできた(っぽい)

--- 火曜5.6限プロジェクト(プロトタイプ)/Assets/Scripts/BGMManager.cs
+++ 火曜5.6限プロジェクト(プロトタイプ)/Assets/Scripts/BGMManager.cs
@@ -9,19 +9,30 @@ public class BGMManager : MonoBehaviour
 
     [SerializeField] private AudioClip[] bgm;
 
-    private AudioSource bgmAudioSource;
+    private AudioSource[] bgmAudioSource;
 
     private void Awake()
     {
         DontDestroyOnLoad(bgmManager);
-        bgmAudioSource = gameObject.GetComponent<AudioSource>();
+        bgmAudioSource[0] = gameObject.GetComponent<AudioSource>();
+        bgmAudioSource[1] = gameObject.GetComponent<AudioSource>();
     }
-    public void BGM_Start()
+    public void BGM()
     {
         if(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "Title")
         {
-            bgmAudioSource.clip = bgm[0];
-            bgmAudioSource.Play();
+            bgmAudioSource[0].clip = bgm[0];
+            bgmAudioSource[0].Play();
+        }
+        else if(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name == "Main")
+        {
+            bgmAudioSource[1].clip = bgm[1];
+            bgmAudioSource[1].Play();
+        }
+        else
+        {
+            bgmAudioSource[0].Stop();
+            bgmAudioSource[1].Stop();
         }
     }
 }
--- 火曜5.6限プロジェクト(プロトタイプ)/Assets/Scripts/SEManager.cs
+++ 火曜5.6限プロジェクト(プロトタイプ)/Assets/Scripts/SEManager.cs
@@ -8,15 +8,43 @@ public class SEManager : MonoBehaviour
 
     [SerializeField]private AudioClip[] se;
 
-    private AudioSource seAudioSource;
+    private AudioSource[] seAudioSource;
     private void Awake()
     {
         DontDestroyOnLoad(seManager);
-        seAudioSource = gameObject.GetComponent<AudioSource>();
+        for (int i = 0; i < se.Length; i++)
+        {
+            seAudioSource[i] = gameObject.GetComponent<AudioSource>();
+        }
     }
-    public void SE()
+    public void SE1()
     {
-        seAudioSource.clip = se[0];
-        seAudioSource.Play();
+        seAudioSource[0].clip = se[0];
+        seAudioSource[0].Play();
+    }
+    public void SE2()
+    {
+        seAudioSource[1].clip = se[1];
+        seAudioSource[1].Play();
+    }
+    public void SE3()
+    {
+        seAudioSource[2].clip = se[2];
+        seAudioSource[2].Play();
+    }
+    public void SE4()
+    {
+        seAudioSource[3].clip = se[3];
+        seAudioSource[3].Play();
+    }
+    public void SE5()
+    {
+        seAudioSource[4].clip = se[4];
+        seAudioSource[4].Play();
+    }
+    public void SE6()
+    {
+        seAudioSource[5].clip = se[5];
+        seAudioSource[5].Play();
     }
 }
